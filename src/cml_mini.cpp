9807f6796c8854f851eeccfd33506d7363754fb4
changephilip <changephilip@github.com>
2017-04-06 21:27:43 -0700

    add cml_mini.cpp

--- src/cml_mini.cpp
+++ src/cml_mini.cpp
@@ -1,1 +1,42 @@
+#include <time.h>
+#include <sys/time.h>
+//#include <array>>
+#include <malloc.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <math.h>
+#include <cblas.h>
+#include <algorithm>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#define L 140
+#define L_power 19600
+/*
+ *this program is used to test cuda-function
+ * 1.can cublas in kernel read data correctly?
+ * 2.can cublas in kernel compute correctly?
+ * 3.the data returened by kernel is correctly?
+ *
+ *
+ *
+ *
+ */
+int main(){
+    FILE *fdata;
+    char *datafilename;
+    datafilename="~/..";
+    fdata=fopen(datafilename,"rb");
+    float *matrix;
+    int N=10;
+    matrix = new float [L_power*N];
 
+
+}
